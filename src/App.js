2d8201b3d297fac2c096af0631dc9d3dfd12cfe4
Juwan H <jhollingsworth5@student.clayton.edu>
2022-05-12 19:51:05 -0400

    wgpt3 feature bar complete

--- src/App.js
+++ src/App.js
@@ -9,24 +9,23 @@ import {
   Header,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibilities />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibilities />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
