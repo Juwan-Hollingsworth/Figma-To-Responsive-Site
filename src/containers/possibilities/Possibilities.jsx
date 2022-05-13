571792241cc95526061ad212a19f3d76b667ba97
Juwan H <jhollingsworth5@student.clayton.edu>
2022-05-12 20:55:57 -0400

    feature section (c) - Possibilites (w)

--- src/containers/possibilities/Possibilities.jsx
+++ src/containers/possibilities/Possibilities.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./possibilities.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibilities = () => {
-  return <div>Possibilities</div>;
+  return (
+    <div className="gpt3__possibilities section__padding" id="possibilities">
+      <div className="gpt3__possibilities-image">
+        <img src={possibilityImage} alt="possiblilities"></img>
+      </div>
+      <div className="gpt3__possibilities-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient-text">The possibi</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Possibilities;
