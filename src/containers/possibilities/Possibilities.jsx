b8561cf1a09f7738e0b86847f8a12dc0361d2749
Juwan <jhollingsworth5@student.clayton.edu>
2023-06-29 20:54:58 -0400

    style css and add media queries

--- src/containers/possibilities/Possibilities.jsx
+++ src/containers/possibilities/Possibilities.jsx
@@ -10,7 +10,16 @@ const Possibilities = () => {
       </div>
       <div className="gpt3__possibilities-content">
         <h4>Request Early Access to Get Started</h4>
-        <h1 className="gradient-text">The possibi</h1>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          facilis ducimus saepe, mollitia velit necessitatibus possimus quos
+          accusamus doloribus magnam vel maiores delectus, sunt autem! Officia
+          iure nobis ab maiores.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
   );
